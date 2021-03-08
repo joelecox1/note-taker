25d2be4baa727a13cbbb7de3fdb3896ebc8e0a0c
Count Zero <joelecox1@gmail.com>
2021-03-07 21:00:59 -0600

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// parse incoming string or arry data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
